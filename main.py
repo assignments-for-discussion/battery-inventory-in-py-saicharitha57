2d2bd031a011e0561dbe6e0669a5e406c7ea90f1
saicharitha57 <136741362+saicharitha57@users.noreply.github.com>
2023-09-08 19:47:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-
 def count_batteries_by_health(present_capacities):
-  return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
-  }
+    counts = {
+        "healthy": 0,
+        "exchange": 0,
+        "failed": 0
+    }
+    
+    rated_capacity = 120
 
+    for i in present_capacities:
+        soh = 100 * i / rated_capacity
+        if soh > 80:
+            counts["healthy"] += 1
+        elif 65 <= soh <= 80:
+            counts["exchange"] += 1
+        elif soh < 65:
+            counts["failed"] += 1
+
+    return counts
 
 def test_bucketing_by_health():
-  print("Counting batteries by SoH...\n")
-  present_capacities = [115, 118, 80, 95, 91, 77]
-  counts = count_batteries_by_health(present_capacities)
-  assert(counts["healthy"] == 2)
-  assert(counts["exchange"] == 3)
-  assert(counts["failed"] == 1)
-  print("Done counting :)")
+    print("Counting batteries by SoH...\n")
+    present_capacities = [115, 118, 80, 95, 91, 77]
+    counts = count_batteries_by_health(present_capacities)
+    assert counts["healthy"] == 2
+    assert counts["exchange"] == 3
+    assert counts["failed"] == 1
+    print("Done counting :)")
+
+if _name_ == '_main_':
+    test_bucketing_by_health()
 
 
-if __name__ == '__main__':
-  test_bucketing_by_health()
